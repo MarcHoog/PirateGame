4470477c20b9184de593f5eac3c2a72815314dcc
Marc Hoogendoorn <bubble@mac.home>
2025-05-17 19:04:48 +0200

    restructed Asset manager

--- canvas.go
+++ canvas.go
@@ -20,6 +20,32 @@ type CanvasTile struct {
 	// objects []*interface{}
 }
 
+func (ct *CanvasTile) RefreshTileSprite(de DataEntry2, am *AssetManager) {
+	ct.sprite.Img.Clear()
+	if ct.hasTerrain {
+		a, err := am.Get("graphics/terrain/land/A")
+		if err == nil {
+			ct.sprite.Img.DrawImage(a.Image, nil)
+		} else {
+			fmt.Printf("error getting asset: %v\n", err)
+		}
+	}
+}
+
+func (ct *CanvasTile) AddIndex(de DataEntry2, selectionIndex int) {
+
+	switch de.Style {
+	case "terrain":
+		ct.hasTerrain = true
+	case "water":
+		ct.hasWater = true
+	case "coin":
+		ct.coin = selectionIndex
+	case "enemy":
+		ct.enemy = selectionIndex
+	}
+}
+
 type Canvas struct {
 	editorData   map[int]DataEntry2
 	assetManager *AssetManager
@@ -42,74 +68,37 @@ func NewCanvas(am *AssetManager) *Canvas {
 
 func (c *Canvas) Draw(origin image.Point, screen *ebiten.Image) {
 	for pos, tile := range c.canvas {
-		pixelPos := origin.Add(pos.Mul(tileSize))
-		if tile.hasTerrain {
-			a, err := c.assetManager.Get("graphics/terrain/land/A")
-			if err == nil {
-				sprite := NewBasicSprite(a.Image, pixelPos)
-				sprite.Draw(screen)
-			} else {
-				fmt.Printf("error getting asset: %v\n", err)
-			}
-
-		}
+		tile.sprite.pos = origin.Add(pos.Mul(tileSize))
+		tile.sprite.Draw(screen)
 	}
 }
 
 func (c *Canvas) NewTile(pos image.Point, selectionIndex int) error {
 
+	dataEntry, ok := c.editorData[selectionIndex]
+	if !ok {
+		return fmt.Errorf("data entry not found for index: %d", selectionIndex)
+	}
+
 	if existingTile, ok := c.canvas[pos]; ok {
-		err := c.AddIndex(existingTile, selectionIndex)
-		if err != nil {
-			fmt.Printf("error updating tile: %v\n", err)
-		}
+		existingTile.AddIndex(dataEntry, selectionIndex)
+		existingTile.RefreshTileSprite(dataEntry, c.assetManager)
 
 	} else {
+		blankImage := ebiten.NewImage(tileSize, tileSize)
 		tile := &CanvasTile{
+			sprite:            NewBasicSprite(blankImage, pos),
 			hasTerrain:        false,
 			terrainNeighbours: []int{},
 			hasWater:          false,
 			waterOnTop:        false,
 		}
 
-		err := c.AddIndex(tile, selectionIndex)
-
-		if err != nil {
-			fmt.Printf("error creating new tile: %v\n", err)
-		} else {
-			c.canvas[pos] = tile
-		}
-
+		tile.AddIndex(dataEntry, selectionIndex)
+		tile.RefreshTileSprite(dataEntry, c.assetManager)
+		c.canvas[pos] = tile
 	}
 
 	fmt.Printf("%v\n", c.canvas)
-	return nil
-}
-
-func (c *Canvas) RefreshTileSprite(pos image.Point) {
-	tile, ok := c.canvas[pos]
-	if !ok {
-		return
-	}
-}
-
-func (c *Canvas) AddIndex(tile *CanvasTile, selectionIndex int) error {
-
-	e, ok := c.editorData[selectionIndex]
-	if !ok {
-		return fmt.Errorf("no entry found for index: %d", selectionIndex)
-	}
-
-	switch e.Style {
-	case "terrain":
-		tile.hasTerrain = true
-	case "water":
-		tile.hasWater = true
-	case "coin":
-		tile.coin = selectionIndex
-	case "enemy":
-		tile.enemy = selectionIndex
-	}
-
 	return nil
 }
